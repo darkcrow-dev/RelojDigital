9269fadf5adb5ea41efa1ac0522c1d4611762750
Charly Lopez <shadowlink1993@hotmail.com>
2023-02-17 19:49:03 -0600

    Agregar archivos de proyecto.

---
+++ RelojDigital.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from time import strftime
+
+def actualizarReloj():
+    hora_minutos.config(text = strftime("%H:%M"))
+    segundos.config(text = strftime("%S"))
+    fecha.config(text = strftime("%A, %d/%m/%Y"))
+    segundos.after(1000, actualizarReloj)
+
+ventana = Tk()
+ventana.title("Reloj digital")
+
+ventanaHoraFecha = Frame()
+ventanaHoraFecha.pack()
+
+hora_minutos = Label(ventanaHoraFecha, font = ("Arial", 100), text = "H:M")
+hora_minutos.grid(row = 0, column = 0)
+
+segundos = Label(ventanaHoraFecha, font = ("Arial", 50), text = "s")
+segundos.grid(row = 0, column = 1, sticky = "n")
+
+fecha = Label(font = ("Arial", 50), text = "dia d/m/aaaa")
+fecha.pack(anchor = "center")
+
+actualizarReloj()
+
+ventana.mainloop()
